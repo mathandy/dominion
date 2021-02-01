a35bed3f5e2276adadb25d58a5b36687a4a08dde
Andrew Port <andyaport@gmail.com>
2021-01-31 21:44:52 -0800

    added support for exiling

--- dominion.py
+++ dominion.py
@@ -1,50 +1,104 @@
-LINE_END_TOKEN = '\n'
-
-# class Player:
-# 	def __init__(self, na):
-
-def get_players(text):
-	token = " starts with "
-	return set(line.split(token)[0] for line in text.split(LINE_END_TOKEN) if token in line)
-
-def get_decks(text, players=None):
-	if players is None:
-		players = get_players(text)
-
-	buy_token = ' buys and gains'
-	gain_token = 'gains '
-	card_removeables = ('.', ' a ', ' an ')
-
-	decks = dict((p, []) for p in players)
-	for line in text.split(LINE_END_TOKEN):
-		buy_token = ' buys and gains'
-		gain_token
-		if buy_token in line:
-			player, card = line.split(buy_token)
-			for r in card_removeables:
-				card = card.replace(r, '')
-			decks[player].append(card)
-	return decks
+from collections import deque
+import re
 
 
-def show_decks(text):
-	decks = get_decks(text)
-	for player, deck in decks.items():
-		print(player)
-		for card in set(deck):
-			print(f'\t{card}: {deck.count(card)}')
+def parse_description_of_cards(cards_text):
+    """Parses a text description of cards into a list of cards.
+    E.g.
+    Given '2 Coppers, a Gold and an Estate.', this function will return
+    ['Copper', 'Copper', 'Gold', 'Estate']
+    """
+    card_removeables = ('.', ' a ', ' an ')
+    card_descriptions = re.split(',|and', cards_text)
+
+    cards = []
+    for card in card_descriptions:
+
+        # clean up card description
+        for r in card_removeables:
+            card = card.replace(r, '')
+        card = card.strip()
+
+        # get count if there's a number at the beginning
+        try:
+            n = int(card.split(' ')[0])
+        except:
+            n = 1
+
+        cards += [card] * n
+    return cards
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.deck = ['Copper'] * 7 + ['Estate'] * 3
+        self.hand = []
+        self.discard_pile = []
+        self.exile = []
+        self.trash = []
+        self.discarded = []
+
+    def show(self):
+        print(self.name)
+        print('\tDeck:')
+        for card in set(self.deck):
+            print(f'\t\t{card}: {self.deck.count(card)}')
+        print('\tExile:')
+        for card in set(self.exile):
+            print(f'\t\t{card}: {self.exile.count(card)}')
+
+    def gain(self, description_of_cards):
+        for card in parse_description_of_cards(description_of_cards):
+            self.deck.append(card)
+
+    def exile_from_supply(self, description_of_cards):
+        for card in parse_description_of_cards(description_of_cards):
+            self.exile.append(card)
+
+    def exile_from_deck(self, description_of_cards):
+        for card in parse_description_of_cards(description_of_cards):
+            self.deck.remove(card)
+            self.exile.append(card)
+
+
+class Game:
+    def __init__(self, log):
+        self.log = log.replace('\r', '')  # in case of windows line endings
+        self.log_lines = self.log.split('\n')
+        # get players
+        token = " starts with "
+        players = set(Player(line.split(token)[0]) for line in self.log_lines
+                      if token in line)
+        self.player_dict = dict((p.name, p) for p in players)
+
+        # get cards
+        buy_token = ' buys and gains'
+        gain_token = ' gains'
+        exile_token = ' exiles'
+        for line in self.log.split('\n'):
+            if buy_token in line:
+                player, description_of_cards = line.split(buy_token)
+                self.player_dict[player.strip()].gain(description_of_cards)
+
+            elif gain_token in line:
+                player, description_of_cards = line.split(gain_token)
+                self.player_dict[player.strip()].gain(description_of_cards)
+
+            elif exile_token in line:
+                player, description_of_cards = line.split(exile_token)
+                self.player_dict[player.strip()].exile_from_deck(description_of_cards)
+
+    def show(self):
+        for player in self.player_dict.values():
+            player.show()
 
 
 if __name__ == '__main__':
-	with open('sample.txt') as f:
-		test_text = f.read()
+    with open('sample.txt') as f:
+        test_text = f.read()
 
-	players = get_players(test_text)
-	print('players:', players, '\n')
-
-	decks = get_decks(test_text)
-	print('decks:', decks, '\n')
-
-	show_decks(test_text)
+    game = Game(log=test_text)
+    game.show()
 
 
